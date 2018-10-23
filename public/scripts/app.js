5b9ac9a8693c88a2e33c04492e9314418b0a4dee
juanda <juandacorreo@gmail.com>
2018-10-23 16:21:18 +0200

    comienzo 23 octubre

---
+++ public/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/* global ReactDOM */
+console.log('Aplicación en ejecución');
+
+var count = 0;
+var addOne = function addOne() {
+  console.log('addOne');
+};
+
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Count: ',
+    count
+  ),
+  React.createElement(
+    'button',
+    { onClick: addOne },
+    '+1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: minusOne },
+    '-1'
+  ),
+  React.createElement(
+    'button',
+    { onClick: reset },
+    'reset'
+  )
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,20 @@
+/* global ReactDOM */
+console.log('Aplicación en ejecución')
+
+let count = 0
+const addOne = () => {
+  console.log('addOne')
+}
+
+const template = (
+  <div>
+    <h1>Count: {count}</h1>
+    <button onClick={addOne}>+1</button>
+    <button onClick={minusOne}>-1</button>
+    <button onClick={reset}>reset</button>
+  </div>
+)
+
+const appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
